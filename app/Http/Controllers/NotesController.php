f4a7dddd4f84748794f65cb2ea64761b2e7a2da2
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-05-15 21:02:29 -0600

    Agregado todos los metodos GET, POST, UPDATE and DELETE

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class NotesController extends Controller
 {
@@ -14,7 +16,7 @@ class NotesController extends Controller
     {
     }
 
-    // Index method
+    // Get all method
     public function index()
     {
         // Connect de Database
@@ -22,5 +24,45 @@ class NotesController extends Controller
         return response()->json($results);
     }
 
-    //
+
+
+    // Get one method
+    public function show($id)
+    {
+        // Connect de Database
+        $results = DB::select('SELECT * FROM note WHERE id = ?', [$id]);
+        return response()->json($results);
+    }
+
+    // Insert method
+    public function store(Request $request)
+    {
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $is_important = $request->input('is_important');
+        $is_private = $request->input('is_private');
+        $pass = $request->input('pass') ?? '';
+
+        $results = DB::insert("INSERT INTO note (title, content, is_important, is_private, pass) VALUES (?, ?, ?, ?, ?) RETURNING *", [$title, $content, $is_important, $is_private, $pass]);
+        return response()->json($results);
+    }
+
+    // Update method
+    public function update(Request $request, $id)
+    {
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $is_important = $request->input('is_important');
+        $is_private = $request->input('is_private');
+        $pass = $request->input('pass') ?? '';
+        $results = DB::update("UPDATE note SET title = ?, content = ?, is_important = ?, is_private = ?, pass = ? WHERE id = ?", [$title, $content, $is_important, $is_private, $pass, $id]);
+        return response()->json($results);
+    }
+
+    // Delete method
+    public function destroy($id)
+    {
+        $results = DB::delete("DELETE FROM note WHERE id = ?", [$id]);
+        return response()->json($results);
+    }
 }
